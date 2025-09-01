472087b65a04db3018ff3886196fec07b18d97cb
Fadli Nofrizal <fadlioppo460@gmail.com>
2025-09-02 02:42:48 +0700

    final testing use nativewind

--- App.tsx
+++ App.tsx
@@ -308,17 +308,3 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
 });
-
-import "./global.css"
-import { Text, View } from "react-native";
- 
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
-}
-
